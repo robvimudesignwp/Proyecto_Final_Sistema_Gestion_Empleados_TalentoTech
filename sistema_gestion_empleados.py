6278bb68271cca24cd60c45a81c6ed68c9151e5a
Robert <robert-870519@hotmail.com>
2024-07-10 17:18:29 -0500

    Creando la clase Area y sus atributos

--- sistema_gestion_empleados.py
+++ sistema_gestion_empleados.py
@@ -49,3 +49,44 @@ class Jefe(Empleado):
           empleados_str = ",".join(self.obtener_empleados_al_mando()) or "Ninguno"
           return super().obtener_informacion_empleados() + f"\nEmpleados al mando: {empleados_str}\n"
      
+## Creando la clase Area
+
+class Area:
+     def __init__(self, nombre, descripcion):
+          self.__nombre = nombre
+          self.__descripcion = descripcion
+          self.empleados = []
+          self.jefe_area = None
+          
+     def insertar_empleado(self, empleado):
+          for trabajador in self.empleados:
+               if trabajador == empleado:
+                    print(f"Empleado {empleado.nombre_completo()} ya está insertado en el área {self.__nombre}.\n")
+                    return
+               self.empleados.append(empleado)
+               print(f"Empleado {empleado.nombre_completo()} insertado en el área {self.__nombre}.\n")
+               
+     def suprimir_empleado(self, nro_identidad):
+          for trabajador in self.empleados:
+               if trabajador == nro_identidad:
+                    self.empleados.remove(trabajador)
+                    print(f"Empleado {trabajador.nombre_completo()} eliminado del área {self.__nombre}.\n")
+                    return
+               print(f"No se encontró ningún empleado identificado con cédula {nro_identidad} en el área {self.__nombre}.\n")
+               
+     def designar_jefe(self, jefe):
+          if isinstance(jefe, Jefe):
+               self.jefe_area = jefe 
+               print(f"Jefe {jefe.nombre_completo()} asignado al área {self.__nombre}.\n")
+          
+     def obtener_jefe_area(self):
+          return self.jefe_area.nombre_completo() if self.jefe_area else "Ninguno\n"
+     
+     def obtener_empleados(self):
+          return [empleado.nombre_completo() for empleado in self.empleados]
+     
+     def obtener_informacion(self):
+          empleados_str = ", ".join(self.obtener_empleados()) or "Ninguno\n"
+          jefe_str = self.obtener_jefe_area()
+          return f"Área: {self.__nombre}, Descripción: {self.__descripcion}\nJefe de Área: {jefe_str}, Empleados: {empleados_str}\n"
+
