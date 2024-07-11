a81e67efc9c4495640d87eefc2004ffc86af910b
Robert <robert-870519@hotmail.com>
2024-07-11 17:56:59 -0500

    Creando el archivo listar_empleados y agregando la clase Trabajador

---
+++ listar_empleados.py
@@ -0,0 +1,9 @@
+## Creando la clase Trabajador
+class Trabajador:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        
+listadoEmpleados = []
+
